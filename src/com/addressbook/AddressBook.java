ea1309ded8aeebf54a0f25f3e4a7ccb9842b26e5
smrutikapatil <smruti.patil17@gmail.com>
2022-03-12 20:56:35 +0530

    Added- Ability to create contact in address book

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,16 @@
+package com.addressbook;
+
+import java.util.List;
+
+public class AddressBook {
+
+	private List<Contact> contacts;
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+}
